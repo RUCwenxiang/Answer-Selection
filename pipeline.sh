3f5dec7c08b2328ac4b255f6095c7bc591e27a3b
WenXiang Mao <maowx@ushareit.com>
2020-12-02 03:58:58 +0000

    update

--- pipeline.sh
+++ pipeline.sh
@@ -1,2 +1,5 @@
-bash run.sh 2>&1 >run.log
+nohup bash run.sh >run.log 2>&1 &
+nohup bash run_mac_bert.sh >run_mac_bert.log 2>&1 &
+grep -E "INFO:tensorflow:Saving dict|INFO:tensorflow:Loss" run.log
+grep -E "INFO:tensorflow:Saving dict|INFO:tensorflow:Loss" run_mac_bert.log
 grep -E "INFO:tensorflow:Saving dict|INFO:tensorflow:Loss" run.log >train_and_eval.result
